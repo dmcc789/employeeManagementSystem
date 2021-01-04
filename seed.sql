e650063e1c6ec519629211755c45b140eb069b57
Deirdre Cruice <dmcruice@gmail.com>
2021-01-03 19:36:24 -0500

    updated sql seed file

--- seed.sql
+++ seed.sql
@@ -6,9 +6,9 @@ VALUE ("Craig", "Smith", 1, 1), ("Jim", "Jones", 2, 1), ("Alex", "Doe", 3, 2);
 
 -- Role-------
 INSERT INTO role (title, salary, department_id)
-VALUE ("Director", 110000, 1), ("Manager", 90000, 1), ("Employee", 70000, 1);
+VALUE ("VP", 160000, 1), ("Director", 110000, 2), ("Manager", 90000, 3), ("Employee", 70000, 3);
 
 -- Departments-----
 INSERT INTO department (name)
-VALUE ("Operations"), ("Sales");
+VALUE ("Executive"), ("Operations"), ("Sales");
 
